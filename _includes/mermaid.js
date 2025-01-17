eba85a1e1a1bc028fcd37ee3711da0903ca6cb43
Dewayne Cowles <dewww@mac.com>
2025-01-17 03:21:32 -0800

    Create mermaid.js

---
+++ _includes/mermaid.js
@@ -0,0 +1,7 @@
+```javascript
+   <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+   <script>
+       document.addEventListener('DOMContentLoaded', () => {
+           mermaid.initialize({ startOnLoad: true });
+       });
+   </script>
